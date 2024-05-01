de727feea3e2f69abc0b6a7a6f95baef761b14c0
ums1212 <voll1212@gmail.com>
2024-05-01 23:52:14 +0900

    [Feat] 첫 화면 프래그먼트

--- app/src/main/java/kr/co/lion/mychat/ui/EnterFragment.kt
+++ app/src/main/java/kr/co/lion/mychat/ui/EnterFragment.kt
@@ -5,56 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kr.co.lion.mychat.R
+import kr.co.lion.mychat.MainActivity
+import kr.co.lion.mychat.databinding.FragmentEnterBinding
+import kr.co.lion.mychat.utils.FragmentName
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EnterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EnterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentEnterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_enter, container, false)
+        binding = FragmentEnterBinding.inflate(inflater)
+
+        settingButton()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EnterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EnterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun settingButton(){
+        with(binding){
+            // 로그인 버튼
+            buttonLogin.setOnClickListener {
+                (activity as MainActivity).replaceFragment(FragmentName.LOGIN_FRAGMENT, true, true, null)
             }
+
+            // 회원가입 버튼
+            buttonRegister.setOnClickListener {
+                (activity as MainActivity).replaceFragment(FragmentName.REGISTER_FRAGMENT, true, true, null)
+            }
+        }
     }
+
 }
