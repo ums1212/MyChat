c5ee0c8d13e44429b71fc6250d9297383c10962d
ums1212 <voll1212@gmail.com>
2024-05-01 23:53:17 +0900

    [Feat] 로그인 프래그먼트

--- app/src/main/java/kr/co/lion/mychat/ui/LoginFragment.kt
+++ app/src/main/java/kr/co/lion/mychat/ui/LoginFragment.kt
@@ -5,56 +5,89 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kr.co.lion.mychat.MainActivity
 import kr.co.lion.mychat.R
+import kr.co.lion.mychat.databinding.FragmentLoginBinding
+import kr.co.lion.mychat.utils.FragmentName
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        binding = FragmentLoginBinding.inflate(inflater)
+
+        settingToolbar()
+        settingButton()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun settingToolbar(){
+        with(binding){
+            toolbarLogin.apply {
+                setNavigationIcon(com.google.android.material.R.drawable.ic_arrow_back_black_24)
+                setNavigationOnClickListener {
+                    (activity as MainActivity).removeFragment(FragmentName.LOGIN_FRAGMENT)
                 }
             }
+        }
+    }
+
+    private fun settingButton(){
+        with(binding){
+            // 로그인 버튼
+            loginButton.setOnClickListener {
+                if(checkLogin()) return@setOnClickListener
+                (activity as MainActivity).removeFragment(FragmentName.LOGIN_FRAGMENT)
+                (activity as MainActivity).removeFragment(FragmentName.ENTER_FRAGMENT)
+                (activity as MainActivity).replaceFragment(FragmentName.MAIN_FRAGMENT, false, true, null)
+            }
+
+            // 회원가입 버튼
+            loginRegisterButton.setOnClickListener {
+                (activity as MainActivity).removeFragment(FragmentName.LOGIN_FRAGMENT)
+                (activity as MainActivity).replaceFragment(FragmentName.REGISTER_FRAGMENT, true, true, null)
+            }
+            // 비밀번호 찾기 버튼
+            findPasswordButton.setOnClickListener {
+
+            }
+
+            // 구글 계정 버튼
+            loginButtonGoogle.setOnClickListener {
+
+            }
+
+            // 네이버 계정 버튼
+            loginButtonNaver.setOnClickListener {
+
+            }
+
+            // 카카오 계정 버튼
+            loginButtonKakao.setOnClickListener {
+
+            }
+        }
+    }
+
+    private fun checkLogin():Boolean{
+        var result = false
+        with(binding){
+            if(textInputPhone.text.isNullOrEmpty()){
+                textInputPhone.error = "핸드폰 번호를 입력해주세요"
+                result = true
+            }
+            if(textInputPassword.text.isNullOrEmpty()){
+                textInputPassword.error = "비밀번호를 입력해주세요"
+                result = true
+            }
+        }
+        return result
     }
 }
