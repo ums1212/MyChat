b98d9bb0c844990734f1171bee1661eda5c2f59b
ums1212 <voll1212@gmail.com>
2024-05-01 23:53:35 +0900

    [Feat] 프래그먼트명 enum class

---
+++ app/src/main/java/kr/co/lion/mychat/utils/FragmentName.kt
@@ -0,0 +1,13 @@
+package kr.co.lion.mychat.utils
+
+enum class FragmentName(var str:String) {
+    ENTER_FRAGMENT("EnterFragment"),
+    LOGIN_FRAGMENT("LoginFragment"),
+    REGISTER_FRAGMENT("RegisterFragment"),
+    REGISTER2_FRAGMENT("Register2Fragment"),
+    REGISTER_COMPLETE_FRAGMENT("RegisterCompleteFragment"),
+    MAIN_FRAGMENT("MainFragment"),
+    CHAT_LIST_FRAGMENT("ChatListFragment"),
+    CHATTING_FRAGMENT("ChattingFragment"),
+    MYPAGE_FRAGMENT("MypageFragment"),
+}
