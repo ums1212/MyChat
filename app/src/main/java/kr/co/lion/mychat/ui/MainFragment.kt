dd0e8fe9e9aa3b538a032e82408cab020a369d76
ums1212 <voll1212@gmail.com>
2024-05-02 01:05:56 +0900

    [Feat] MainFragment TabLayout 화면 이동 작업

--- app/src/main/java/kr/co/lion/mychat/ui/MainFragment.kt
+++ app/src/main/java/kr/co/lion/mychat/ui/MainFragment.kt
@@ -1,60 +1,125 @@
 package kr.co.lion.mychat.ui
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.transition.MaterialSharedAxis
 import kr.co.lion.mychat.R
+import kr.co.lion.mychat.databinding.FragmentMainBinding
+import kr.co.lion.mychat.utils.FragmentName
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentMainBinding
+
+    // 프래그먼트 객체를 담을 변수
+    var oldMainFragment: Fragment? = null
+    var newMainFragment: Fragment? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        binding = FragmentMainBinding.inflate(inflater)
+
+        settingTabLayout()
+        replaceFragment(FragmentName.CHAT_LIST_FRAGMENT, false, false, null)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun settingTabLayout(){
+        binding.tabLayoutMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    when(tab.position){
+                        0 -> {
+                            replaceFragment(FragmentName.CHAT_LIST_FRAGMENT, false, false, null)
+                        }
+
+                        1 ->{
+                            replaceFragment(FragmentName.MYPAGE_FRAGMENT, false, false, null)
+                        }
+                    }
                 }
             }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
+
+    fun replaceFragment(name: FragmentName, addToBackStack:Boolean, isAnimate:Boolean, data:Bundle?){
+        SystemClock.sleep(200)
+
+        val fragmentTransaction = childFragmentManager.beginTransaction()
+
+        if(newMainFragment != null){
+            oldMainFragment = newMainFragment
+        }
+
+        newMainFragment = when(name){
+            FragmentName.ENTER_FRAGMENT -> { EnterFragment() }
+            FragmentName.LOGIN_FRAGMENT -> { LoginFragment() }
+            FragmentName.REGISTER_FRAGMENT -> { RegisterFragment() }
+            FragmentName.REGISTER2_FRAGMENT -> { Register2Fragment() }
+            FragmentName.REGISTER_COMPLETE_FRAGMENT -> { RegisterCompleteFragment() }
+            FragmentName.MAIN_FRAGMENT -> { MainFragment() }
+            FragmentName.CHAT_LIST_FRAGMENT -> { ChatListFragment() }
+            FragmentName.CHATTING_FRAGMENT -> { ChattingFragment() }
+            FragmentName.MYPAGE_FRAGMENT -> { MypageFragment() }
+        }
+
+        if(data != null){
+            newMainFragment?.arguments = data
+        }
+
+        if(newMainFragment != null){
+            if(isAnimate){
+                // MaterialSharedAxis : 좌우, 위아래, 공중 바닥 사이로 이동하는 애니메이션 효과
+                // X - 좌우
+                // Y - 위아래
+                // Z - 공중 바닥
+                // 두 번째 매개변수 : 새로운 화면이 나타나는 것인지 여부를 설정
+                // true : 새로운 화면이 나타나는 애니메이션이 동작한다.
+                // false : 이전으로 되돌아가는 애니메이션이 동작한다.
+                if(oldMainFragment != null){
+                    // old에서 new가 새롭게 보여질 때 old의 애니메이션
+                    oldMainFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                    // new에서 old로 되돌아갈 때 old의 애니메이션
+                    oldMainFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+
+                    oldMainFragment?.enterTransition = null
+                    oldMainFragment?.returnTransition = null
+                }
+
+                // old에서 new가 새롭게 보여질 때 new의 애니메이션
+                newMainFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                // new에서 old로 되돌아갈 때의 애니메이션
+                newMainFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+
+                newMainFragment?.exitTransition = null
+                newMainFragment?.reenterTransition = null
+            }
+
+            fragmentTransaction.replace(R.id.fragmentContainerViewMain, newMainFragment!!)
+
+            if(addToBackStack == true){
+                fragmentTransaction.addToBackStack(name.str)
+            }
+
+            fragmentTransaction.commit()
+        }
+    }
+
 }
